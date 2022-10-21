2cc43a59bf4e1dd64582aaca36869ed2e06db2a3
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-10-20 21:25:28 -0500

    Update estudiante.controller.js

--- controllers/estudiante.controller.js
+++ controllers/estudiante.controller.js
@@ -1,7 +1,15 @@
-const Estudiante = require('../models/estudiante.model')
+const Estudiante = require("../models/estudiante.model");
 
+//Regresa lista de objetos/documentos Estudiante
 module.exports.muestraEstudiantes = (req, res) => {
     Estudiante.find()
-        .then(todosEstudiantes => res.json({estudiantes: estudiantes}))
-        .catch(err => res.json({message: 'Something went wrong', error: err}))
-}
+        .then(todosEstudiantes => res.json({estudiantes: todosEstudiantes}))
+        .catch(err => res.json({message: "Something went wrong", error: err}))
+};
+
+//Me regresa solo un documento de estudiante en base al ID que mandemos en la URL
+module.exports.muestraUnEstudiante = (req, res) => {
+    Estudiante.findOne({_id: req.params.id})
+        .then(unEstudiante => res.json({estudiante: unEstudiante}))
+        .catch(err => res.json({message: "Something went wrong", error: err}))
+};
