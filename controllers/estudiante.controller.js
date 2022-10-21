cba3117c13bb383f96fceba27fb7a1bd7a73606b
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-10-20 21:49:45 -0500

    added routes

--- controllers/estudiante.controller.js
+++ controllers/estudiante.controller.js
@@ -13,3 +13,24 @@ module.exports.muestraUnEstudiante = (req, res) => {
         .then(unEstudiante => res.json({estudiante: unEstudiante}))
         .catch(err => res.json({message: "Something went wrong", error: err}))
 };
+
+//Me crea un nuevo estudiante en base a lo que reciba en body
+module.exports.creaEstudiante = (req, res) => {
+    Estudiante.create(req.body)
+        .then(nuevoEstudiante => res.json({estudiante: nuevoEstudiante}))
+        .catch(err => res.json({message: "Something went wrong", error: err}))
+}
+
+//Me actualiza el estudiante en base a lo que reciba en body y lo que reciba en id URL
+module.exports.actualizaEstudiante = (req, res) => {
+    Estudiante.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+        .then(estudianteActualizado => res.json({estudiante: estudianteActualizado}))
+        .catch(err => res.json({message: "Something went wrong", error: err}))
+}
+
+//Borramos un estudiante en base al ID que tenemos en la URL
+module.exports.borrarEstudiante = (req, res) => {
+    Estudiante.deleteOne({_id: req.params.id})
+        .then(result => res.json({result: result}))
+        .catch(err => res.json({message: "Something went wrong", error: err}))
+}
